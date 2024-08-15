e419c162ba70a79281ba3f3ec47695a95a28af56
Rafael Kitover <rkitover@gmail.com>
2024-08-15 08:42:15 +0000

    Improve example to use a single WMI query
    Use a single WMI query to get the parent process id and its commandline
    instead of using ToolHelper to get the ppid and iterating over the set
    of all processes.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- show-parent.cpp
+++ show-parent.cpp
@@ -9,7 +9,6 @@
 
 #include <windows.h>
 #include <comutil.h>
-#include <tlhelp32.h>
 #include <wbemidl.h>
 
 // From: https://stackoverflow.com/a/20082113/262458
@@ -19,57 +18,46 @@ main(int argc, char **argv)
 {
 	int			 pid = argv[1] ? atoi(argv[1]) : GetCurrentProcessId();
 	int			 ppid = -1;
-	HANDLE			 h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 	HRESULT			 hr = E_FAIL;
-	PROCESSENTRY32		 pe = { 0 };
 	IWbemLocator		*wbem_locator  = NULL;
 	IWbemServices		*wbem_services = NULL;
 	IEnumWbemClassObject	*enum_wbem  = NULL;
-
-	pe.dwSize = sizeof(PROCESSENTRY32);
-
-	if (Process32First(h, &pe)) {
-		do {
-			if (pe.th32ProcessID == pid)
-				ppid = pe.th32ParentProcessID;
-		} while (Process32Next(h, &pe));
-	}
-
-	CloseHandle(h);
 
 	CoInitializeEx(0, COINIT_MULTITHREADED);
 	CoInitializeSecurity(NULL, -1, NULL, NULL, RPC_C_AUTHN_LEVEL_DEFAULT, RPC_C_IMP_LEVEL_IMPERSONATE, NULL, EOAC_NONE, NULL);
 	CoCreateInstance(CLSID_WbemLocator, 0, CLSCTX_INPROC_SERVER, IID_IWbemLocator, (LPVOID *) &wbem_locator);
 
 	wbem_locator->ConnectServer(L"ROOT\\CIMV2", NULL, NULL, NULL, 0, NULL, NULL, &wbem_services);
-	wbem_services->ExecQuery(L"WQL", L"SELECT ProcessId,CommandLine FROM Win32_Process", WBEM_FLAG_FORWARD_ONLY, NULL, &enum_wbem);
+	wchar_t *query = new wchar_t[4096];
+	swprintf(query, 4096,
+L"select processid, commandline from win32_process where processid = (select parentprocessid from win32_process where processid = %d)", pid);
+	wbem_services->ExecQuery(L"WQL", query, WBEM_FLAG_FORWARD_ONLY, NULL, &enum_wbem);
+	delete[] query;
 
 	if (enum_wbem != NULL) {
 		IWbemClassObject *result = NULL;
 		ULONG returned_count = 0;
 
-		while((hr = enum_wbem->Next(WBEM_INFINITE, 1, &result, &returned_count)) == S_OK) {
+		if((hr = enum_wbem->Next(WBEM_INFINITE, 1, &result, &returned_count)) == S_OK) {
 			VARIANT process_id;
 			VARIANT command_line;
 
 			result->Get(L"ProcessId", 0, &process_id, 0, 0);
+			ppid = process_id.uintVal;
+
 			result->Get(L"CommandLine", 0, &command_line, 0, 0);
 
-			if (process_id.uintVal == ppid)
-			{
-				wchar_t		*command_line_utf16 = command_line.bstrVal;
-				size_t		 size = wcslen(command_line_utf16) + 1;
-				char		*command_line_utf8 = new char[size];
+			wchar_t		*command_line_utf16 = command_line.bstrVal;
+			size_t		 size = wcslen(command_line_utf16) + 1;
+			char		*command_line_utf8 = new char[size];
 
-				wcstombs(command_line_utf8, command_line_utf16, size);
+			wcstombs(command_line_utf8, command_line_utf16, size);
 
-				SysFreeString(command_line_utf16);
+			SysFreeString(command_line_utf16);
 
-				printf("%d: %d: %s\n", pid, ppid, command_line_utf8);
+			printf("%d: %d: %s\n", pid, ppid, command_line_utf8);
 
-				delete command_line_utf8;
-				break;
-			}
+			delete command_line_utf8;
 
 			result->Release();
 		}
